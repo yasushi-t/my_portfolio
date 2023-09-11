99bfa9f5da8285f48e38af1f1252e8fdb561f729
yasushitanaka <caza.y.tanaka@gmail.com>
2023-09-11 22:18:49 +0900

    middleware.tsとauth.tsを追加

---
+++ src/pages/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
+
+  if (process.env.NODE_ENV === 'production') {
+    if (basicAuth) {
+      const authValue = basicAuth.split(' ')[1];
+      const [user, pwd] = atob(authValue).split(':');
+
+      if (user === '4dmin' && pwd === 'testpwd123') {
+        return NextResponse.next();
+      }
+    }
+    url.pathname = '/api/auth';
+
+    return NextResponse.rewrite(url);
+  }
+}
